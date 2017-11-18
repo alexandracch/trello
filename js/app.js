4a46bd08e213393e9b4bbe6a870b0aa429d4b091
alexandracch <alexa_theprinces@hotmail.com>
2017-11-18 11:21:31 -0500

    primer evento iniciado

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 //lista 1 div
+var divLista1 = document.getElementById('divlista1');
+
+divLista1.addEventListener('click', function(event) {
+  var newDiv = document.createElement('div');
+
+  divLista1.classList.add('display-div');
+  
+
+});
