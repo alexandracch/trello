f9a98abba676a6c8640951390a3965c704be6e46
alexandracch <alexa_theprinces@hotmail.com>
2017-11-19 03:01:41 -0500

    versión 0.0.1

--- js/app.js
+++ js/app.js
@@ -1,10 +1,33 @@
-//lista 1 div
+// lista 1 div
 var divLista1 = document.getElementById('divlista1');
+// lista-change
+var divListaChange = document.getElementById('div-change');
+// boton guardar
+var botonGuardar = document.getElementById('boton-guardar');
+// input
+var input = document.getElementById('input');
+// ul
+var listaTareas = document.getElementById('lista-tareas')
+
+console.log(listaTareas);
 
 divLista1.addEventListener('click', function(event) {
-  var newDiv = document.createElement('div');
-
   divLista1.classList.add('display-div');
-  
-
+  divListaChange.classList.add('visible')
 });
+
+botonGuardar.addEventListener('click', function(event) {
+  divListaChange.classList.add('movimiento');
+  listaTareas.classList.add('visible');
+  
+  if (input.value) { 
+    var li = document.createElement('li');
+    var tarea = document.createElement('p'); 
+    tarea.textContent = input.value;
+    tarea.classList.add('li'); 
+    li.appendChild(tarea);
+    listaTareas.appendChild(tarea);
+    input.value = ' ';  
+  }
+});
+
