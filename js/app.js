23c8bcef97cef5ec9be192c842a6371ff70f533b
alexandracch <alexa_theprinces@hotmail.com>
2017-11-20 01:44:46 -0500

    avance

--- js/app.js
+++ js/app.js
@@ -6,10 +6,10 @@ var divListaChange = document.getElementById('div-change');
 var botonGuardar = document.getElementById('boton-guardar');
 // input
 var input = document.getElementById('input');
-// ul
-var listaTareas = document.getElementById('lista-tareas')
+// main
+var main = document.body.children[2];
 
-console.log(listaTareas);
+console.log(main);
 
 divLista1.addEventListener('click', function(event) {
   divLista1.classList.add('display-div');
@@ -17,44 +17,37 @@ divLista1.addEventListener('click', function(event) {
 });
 
 botonGuardar.addEventListener('click', function(event) {
+  // Para que desaparezcan elementos y se muevan
   divLista1.classList.add('ocultar');
   divListaChange.classList.toggle('visible');
   divListaChange.classList.add('movimiento');
-  listaTareas.classList.add('visible');
-  
-  if (input.value) { 
-    var li = document.createElement('li');
-    var atributo = document.createAttribute('href');
-    var divLi = document.createElement('div');
-    var divAgregar = document.createElement('div');
-    var tarea = document.createElement('p'); 
-    var anchor = document.createElement('p');
-    
-    anchor.textContent = 'Añadir tarjeta...';
-    anchor.classList.add('anchor-decoration')
-    divAgregar.appendChild(anchor);
-    divAgregar.classList.add('agregar-tarjeta');
-    divAgregar.classList.add('agregar-tarjeta:hover');
-    divLi.appendChild(divAgregar);
-    divLi.appendChild(tarea);
-    divLi.classList.add('div-tarea');
-    listaTareas.appendChild(divLi);
+  // div contenedor
+  var crearDiv = document.createElement('div');
+  // div contenedor de la nueva tarea
+  var nuevaTarea = document.createElement('div');
+  // obtener lo escrito en el input
+  var tarea = document.createElement('p');
+  // barra de abajo
+  var nuevaTarjeta = document.createElement('p');
 
-    tarea.textContent = input.value;
-    
-    li.appendChild(tarea);
-    listaTareas.appendChild(tarea);
-    input.value = ' ';  
+  //ahora la funcion en sí
+  if (input.value) {
+
+  // para añadir nueva tarjeta
+  nuevaTarjeta.textContent = 'Añadir tarjeta...';
+  nuevaTarjeta.classList.add('anchor-decoration');
+  nuevaTarjeta.classList.add('agregar-tarjeta');
+  nuevaTarjeta.classList.add('agregar-tarjeta');
+  //agregamos la tarea
+  tarea.textContent = input.value;
+  tarea.classList.add('tarea');
+  //ponemos los 2 hijos al div
+  crearDiv.appendChild(tarea);
+  crearDiv.appendChild(nuevaTarjeta);
+  crearDiv.classList.add('div-tarea');
+  //agregamos el div 
+  main.appendChild(crearDiv);
+  input.value = ' ';  
   }
 });
 
-/*<li>
-                <div class="div-tarea">
-
-                    <p>Hola</p>
-                    <div class="agregar-tarjeta">
-                        <a class="a" href="#">Añadir una tarjeta...</a>   
-                    </div>
-                    
-                </div>
-            </li> */
